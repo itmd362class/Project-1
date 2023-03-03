49057391cd278d74a15d8b83e28cd7c9472517cc
Zufsha <zkhan26@hawk.iit.edu>
2023-03-02 23:04:02 -0600

    Create mouseover.js

---
+++ js/mouseover.js
@@ -0,0 +1,9 @@
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("mouseover", function() {
+  submitButton.style.backgroundColor = "grey";
+});
+
+submitButton.addEventListener("mouseout", function() {
+  submitButton.style.backgroundColor = "";
+});
